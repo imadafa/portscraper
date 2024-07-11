b6c48d177ded3c0a5964c18b0f691d262143b03e
adafa <145224550+imadafa@users.noreply.github.com>
2024-07-11 18:09:03 -0400

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+mod portthing;
+use crate:: portthing::scanner::scan;
+use crate:: portthing::scanner::ip;
+use crate:: portthing::scanner::pingG;
+use crate::portthing::scanner::otherthing;
+use std::io;
+
+
+fn main(){
+    otherthing();
+   loop{
+    
+    
+    println!("\n \r[-ip] Show IP\n [-s] PortScan\n [-pg] Ping google \n [-h] help");
+    
+    let mut input = String::new();
+
+    io::stdin()
+     .read_line(&mut input)
+     .expect("ENTER A NUMBER");
+
+    let input: &str = &input.trim();
+    match input{
+        "-h" => println!("HELP COSTS MONEY BROKEY!"),
+        "-s" => scan(),
+        "-pg" => pingG(),
+        "-ip" => ip(),
+        &_ => todo!()
+    }
+    //break;
+    
+
+   }
+}
+  
